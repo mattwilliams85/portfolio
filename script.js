ff4eefb425c1b0cf5c72332f285544421b1d795e
mattwilliams85 <mattwilliams85@gmail.com>
2015-07-25 14:18:45 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+  $('.p1').on('click', function(){
+    $('.p1').addClass('extend')
+    $('.p2, .p4').addClass('shutter')
+  });
+
+  $('.p2, .p3').on('click', function(){
+    var id = parseInt($(this).attr('id'));
+    shutterPanels(id);
+  });
+
+  $('.p4').on('click', function(){
+    $('.p3').addClass('shutter')
+    $('.p1').addClass('retract')
+  });
+
+  $('.logo').on('click', function(){
+    returnPanels();
+  });
+
+
+  function shutterPanels(id) {
+    var n = id;
+    for (var i = 0; i < 4; i++) {
+      n += 1;
+      if (n === 5) n = 1;
+      if (n === id) continue;
+      $('#'+n).addClass('shutter');
+    }
+  }
+
+  function returnPanels() {
+    for (var i = 1; i < 5; i++) {
+      $('#'+i).removeClass('shutter')
+    }
+    $('.p1').removeClass('extend retract')
+  }
+});
