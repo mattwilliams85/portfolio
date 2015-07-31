c2f64da204109d0441084d4cb6fcd85b72aeed39
mattwilliams85 <mattwilliams85@gmail.com>
2015-07-31 10:25:39 -0700

    adds compiled sass files; adds iceberg update

--- script.js
+++ script.js
@@ -12,26 +12,41 @@ $(document).ready(function(){
 
   $('.p3').on('click', function(){
     var id = parseInt($(this).attr('id'));
-    $(this).addClass('extend')
+    $(this).addClass('extend');
     shutterPanels(id);
   });
 
   $('.p4').on('click', function(){
-    $('.p3').addClass('shutter')
-    $('.p1').addClass('retract')
-    $('.p2').addClass('clear')
+    $('.p3').addClass('shutter');
+    $('.p1').addClass('retract');
+    $('.p2').addClass('clear');
+  });
+
+  $('.panel').on('click', function(){
+    if ($('.panel').hasClass('start')) return;
+    if ($('.panel').hasClass('active')) {
+      returnPanels();
+    } else {
+      $('.panel').toggleClass('active');
+    }
   });
 
   $('.logo').on('click', function(){
     if ($('.p1, .p2, .p3, .p4').hasClass('shutter')) {
       returnPanels();
     } else {
+      $('.p3').addClass('start');
       shutterPanels(0);
     }
   });
 
+  $('.logo').on('mouseenter mouseleave', function(){
+    $('.landing-sect').toggleClass('active');
+  });
+
 
   function shutterPanels(id) {
+    if (!id) $('.landing-sect').removeClass('shutter');
     var n = id;
     for (var i = 0; i < 4; i++) {
       n += 1;
@@ -42,8 +57,9 @@ $(document).ready(function(){
   }
 
   function returnPanels() {
+    $('.landing-sect').addClass('shutter');
     for (var i = 1; i < 5; i++) {
-      $('#'+i).removeClass('shutter clear extend retract')
+      $('#'+i).removeClass('shutter clear extend retract start active')
     }
   }
 });
